59420cdc0e5f83bd8188a36e4eb03e1fac5d999d
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-08-11 12:16:27 -0700

    close svg icon added to overlay

--- client/src/components/Overlay.jsx
+++ client/src/components/Overlay.jsx
@@ -1,10 +1,14 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import closeIcon from '../assets/close-icon.svg';
+import sqlLogo from '../assets/sql-logo.png';
 
 const Overlay = (props) => {
 
     return (
       <div className={props.isOpen ? "hide-overlay" : "overlay"}>
-        <button className="close-overlay" type="button" onClick={props.onClose}>X</button>
+        <button className="close-overlay" type="button" onClick={props.onClose}>
+          <img src={closeIcon}/>
+        </button>
         <div className="overlay-nav-links">
           <NavLink>Projects</NavLink>
           <NavLink>LinkedIn</NavLink>
