be9b561ded2adbe471898dd887cf4be0e8256c46
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-08-11 22:27:51 -0700

    update vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'dev-portfolio-2025-production-b166.up.railway.app'
+    ],
+    port: process.env.PORT || 4173,
+    host: '0.0.0.0'
+  }
 })
