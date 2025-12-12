a10a17deaaf7ad33eb90cb5b841b5af1d23666af
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-12-11 17:06:14 -0700

    updated descriptions and links for projects page

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -36,13 +36,11 @@ const Projects = ({ children }) => {
                 <div className="card-content">
                     <h2>fullstack-course-manager</h2>
                     <p>
-                        With user-centered approach, the goals was to create an
-                        intuitive interface for effortless financial management while
-                        incorporating gamification.
+                        This site mimics a school database. It allows users to create, read, update, and delete courses. The site contains a frontend built with React, a REST API on Express, and a simple db with sqlite.
                     </p>
                     <div className="button-wrapper">
-                        <button>Live Site</button>
-                        <button>GitHub Link</button>
+                        <a href="https://desirable-smile-production.up.railway.app/" target="_blank" rel="noopener noreferrer"><button>Live Site</button></a>
+                        <a href="https://github.com/sgaona21/fullstack-course-manager.git" target="_blank" rel="noopener noreferrer"><button>GitHub Link</button></a>
                     </div>
                     <div className="thumbnail-container">
                         <img src={placeholder2} alt="thumbnail of project" />
@@ -54,13 +52,11 @@ const Projects = ({ children }) => {
                 <div className="card-content">
                     <h2>PokerStatTracker</h2>
                     <p>
-                        With user-centered approach, the goals was to create an
-                        intuitive interface for effortless financial management while
-                        incorporating gamification.
+                        This Android application allows users to create and track poker session data. It keeps a log of buy in amounts, cash out amounts, and total profit over time. I built this app for a final project in college.
                     </p>
                     <div className="button-wrapper">
-                        <button>Live Site</button>
-                        <button>GitHub Link</button>
+                        {/* <button>Live Site</button> */}
+                        <a href="https://github.com/sgaona21/PokerSessionTracker.git" target="_blank" rel="noopener noreferrer"><button>GitHub Link</button></a>
                     </div>
                     <div className="thumbnail-container">
                         <img src={placeholder3} alt="thumbnail of project" />
@@ -73,13 +69,12 @@ const Projects = ({ children }) => {
                 <div className="card-content">
                     <h2>Junior Portfolio</h2>
                     <p>
-                        With user-centered approach, the goals was to create an
-                        intuitive interface for effortless financial management while
-                        incorporating gamification.
+                        This was the first portfolio I created after completing my first techdegree from treehouse. It contains a collection of static web applications that demonstrate the basics of responsive design, CSS, and JavaScript.
                     </p>
                     <div className="button-wrapper">
-                        <button>Live Site</button>
-                        <button>GitHub Link</button>
+                        <a href="https://sgaona21.github.io/" target="_blank" rel="noopener noreferrer"><button>Live Site</button></a>
+                        <a href="https://github.com/sgaona21/sgaona21.github.io.git" target="_blank" rel="noopener noreferrer"><button>GitHub Link</button></a>
+                        
                     </div>
                     <div className="thumbnail-container">
                         <img src={placeholder4} alt="thumbnail of project" />
